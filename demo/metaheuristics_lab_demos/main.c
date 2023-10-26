b2d9bddb40884f6331ab991e09d3de2005a924a1
mundus <mundus@pc6.home>
2023-10-27 00:19:44 +0200

    Created demo

---
+++ demo/metaheuristics_lab_demos/main.c
@@ -0,0 +1,17 @@
+#include "data/r2_r_example_functions.h";
+#include "sa/sa_r2_r_basic.h"
+
+void ten_cones_demo() {
+    sa_config_t saConfig;
+    //saConfig.guess = (void*)
+}
+
+void fakir_sin_demo() {
+    
+}
+
+int main() {
+    
+    
+    return 0;
+}
