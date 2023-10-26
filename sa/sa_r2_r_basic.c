c6ea0b056d9bea87b4ead100781cbe1e7ad5886c
mundus <mundus@pc6.home>
2023-10-26 20:54:14 +0200

    Implement comparer and metric in sa_r2_r_basic.c

--- sa/sa_r2_r_basic.c
+++ sa/sa_r2_r_basic.c
@@ -1,4 +1,22 @@
+#include <stdlib.h>
 #include "sa_r2_r_basic.h"
+
+typedef struct {
+    double x;
+    double y;
+} r2_cart_t;
+
+r2_cart_t neighbour(r2_cart_t r2Cart) {
+    //{{implement}}
+}
+
+CMP_RESULT comparer(double ra, double rb) {
+    return rb > ra;
+}
+
+double metric(double ra, double rb) {
+    return abs(rb - ra);
+}
 
 double sa_r2_r_basic_extreme(const sa_config_t config,
                        const saFunc f, 
