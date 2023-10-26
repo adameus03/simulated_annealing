2a13c4d81a5527710fe3a01271ab65ead460f796
mundus <mundus@pc6.home>
2023-10-26 22:27:21 +0200

    sa_r2_r_basic implementation

--- sa/sa_r2_r_basic.c
+++ sa/sa_r2_r_basic.c
@@ -1,3 +1,6 @@
+#define __USE_MISC
+#include <math.h>
+#include <time.h>
 #include <stdlib.h>
 #include "sa_r2_r_basic.h"
 
@@ -5,21 +8,53 @@ typedef struct {
     double x;
     double y;
 } r2_cart_t;
+/*typedef r2_cart_t (*sa_R2RBasic_FuncTDependent)(const r2_cart_t, const double);
+typedef double (*sa_R2RBasic_ScalarFunc)(const double, const double);
+typedef CMP_RESULT (*sa_R2RBasic_Predicate)(const double, const double);*/
 
-r2_cart_t neighbour(r2_cart_t r2Cart) {
+/**
+ * @brief Run the simulated annealing metaheuristic algorithm for a R2->R function.
+ * @param config Structure instance containing algorithm's initial conditions 
+ * and iterations/epochs count
+ * @param f The R2->R function to be optimized
+ * @param emode Used to choose whether the algorithm shoud seek 
+ * the minimum or maximum of the f function
+*/
+void* neighbour(const void* r2Cart, const double temperature) {
     //{{implement}}
+    r2_cart_t* _r2Cart = (r2_cart_t*)r2Cart;
+    static r2_cart_t* _neighR2Cart;
+    _neighR2Cart = malloc(0x1);
+
+    static double distance;
+    srand((unsigned int)time(NULL));
+    distance = 2 * temperature * (double)rand() / RAND_MAX;
+    static double dirAngle;
+    dirAngle = 2 * M_PI * (double)rand() / RAND_MAX;
+    _neighR2Cart->x = _r2Cart->x;
+    _neighR2Cart->y = _r2Cart->y;
+    _neighR2Cart->x += distance * cos(dirAngle);
+    _neighR2Cart->y += distance * sin(dirAngle);
+    
+    return (void*)_neighR2Cart;
 }
 
-CMP_RESULT comparer(double ra, double rb) {
-    return rb > ra;
+CMP_RESULT comparer(const void* ra, const void* rb) {
+    return (*(double*)rb) > *((double*)ra);
 }
 
-double metric(double ra, double rb) {
-    return abs(rb - ra);
+double metric(const void* ra, const void* rb) {
+    return fabs(*(double*)(rb) - *(double*)(ra));
 }
 
 double sa_r2_r_basic_extreme(const sa_config_t config,
                        const saFunc f, 
                        const EXTREME_MODE emode) {
-    
+
+    return *(double*)sa_extreme(config, 
+                                f, 
+                                emode, 
+                                neighbour, 
+                                comparer, 
+                                metric);
 }
--- sa/sa_r2_r_basic.h
+++ sa/sa_r2_r_basic.h
@@ -1,5 +1,13 @@
 #include "sa.h"
 
+/**
+ * @brief Run the simulated annealing metaheuristic algorithm for a R2->R function.
+ * @param config Structure instance containing algorithm's initial conditions 
+ * and iterations/epochs count
+ * @param f The R2->R function to be optimized
+ * @param emode Used to choose whether the algorithm shoud seek 
+ * the minimum or maximum of the f function
+*/
 double sa_r2_r_basic_extreme(const sa_config_t config,
                        const saFunc f, 
                        const EXTREME_MODE emode);
