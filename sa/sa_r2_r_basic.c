cdc5a1ffc8e51ac40d003d8f11edfd553a9bcde7
mundus <mundus@pc6.home>
2023-10-30 21:51:08 +0100

    Ditched malloc

--- sa/sa_r2_r_basic.c
+++ sa/sa_r2_r_basic.c
@@ -17,20 +17,20 @@ typedef CMP_RESULT (*sa_R2RBasic_Predicate)(const double, const double);*/
 */
 void* neighbour(const void* r2Cart, const double temperature) {
     r2_cart_t* _r2Cart = (r2_cart_t*)r2Cart;
-    static r2_cart_t* _neighR2Cart;
-    _neighR2Cart = malloc(0x1);
-
+    //static r2_cart_t* _neighR2Cart;
+    //_neighR2Cart = malloc(0x1);
+    static r2_cart_t _neighR2Cart;
     static double distance;
     srand((unsigned int)time(NULL));
     distance = 2 * temperature * (double)rand() / RAND_MAX;
     static double dirAngle;
     dirAngle = 2 * M_PI * (double)rand() / RAND_MAX;
-    _neighR2Cart->x = _r2Cart->x;
-    _neighR2Cart->y = _r2Cart->y;
-    _neighR2Cart->x += distance * cos(dirAngle);
-    _neighR2Cart->y += distance * sin(dirAngle);
+    _neighR2Cart.x = _r2Cart->x;
+    _neighR2Cart.y = _r2Cart->y;
+    _neighR2Cart.x += distance * cos(dirAngle);
+    _neighR2Cart.y += distance * sin(dirAngle);
     
-    return (void*)_neighR2Cart;
+    return (void*)&_neighR2Cart;
 }
 
 CMP_RESULT comparer(const void* ra, const void* rb) {
